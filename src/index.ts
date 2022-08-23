6057054b9e11be21b4d731968396ff49630a6e7f
Harpalsinh <jadejaharpal14@gmail.com>
2022-08-23 12:56:14 +0530

    celoscan verification working

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-import "@nomiclabs/hardhat-etherscan";
+import "@nomicfoundation/hardhat-toolbox";
 import { extendConfig } from "hardhat/config";
 import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
 
 extendConfig(
   (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
-    const defaultNetwork = userConfig.defaultNetwork
-      ? userConfig.defaultNetwork
-      : "alfajores";
-    config.defaultNetwork = defaultNetwork;
     config.etherscan.customChains.push({
       network: "alfajores",
       chainId: 44787,
       urls: {
-        apiURL: "https://api.celoscan.xyz/api",
-        browserURL: "https://celoscan.xyz",
+        apiURL: "https://api-alfajores.celoscan.io/api",
+        browserURL: "https://alfajores.celoscan.io",
+      },
+    });
+
+    config.etherscan.customChains.push({
+      network: "celo",
+      chainId: 44220,
+      urls: {
+        apiURL: "https://api.celoscan.io/api",
+        browserURL: "https://celoscan.io",
       },
     });
   }
